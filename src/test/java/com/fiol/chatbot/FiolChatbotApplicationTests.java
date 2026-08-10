f51e072b9a542589bd4323d4a1cbc19ea1ee0e14
Alvaro Marina <alvarommarina14@hotmail.com>
2026-08-10 17:31:53 -0300

    chore: scaffold Spring Boot application with Gradle wrapper

---
+++ src/test/java/com/fiol/chatbot/FiolChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiol.chatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiolChatbotApplicationTests {
+
+	@Test
+	void givenApplicationConfiguration_whenStartingContext_thenAllBeansWireUp() {
+	}
+
+}
